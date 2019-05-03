d78e9fa110d15c4a3ab22b835d3f09a6000c21ac
certeu <cert-eu@ec.europa.eu>
2019-05-03 14:44:23 +0200

    Add pagination support to Deliverables list

--- app/scripts/controllers/deliverables.js
+++ app/scripts/controllers/deliverables.js
@@ -8,9 +8,22 @@
  * Controller of the cpApp
  */
 angular.module('cpApp')
-  .controller('DeliverablesCtrl', function ($scope, GridData, config) {
-    GridData('files').query(function(resp){
-      $scope.files = resp.items;
-      $scope.webServiceUrl = config.apiConfig.webServiceUrl;
-    });
+  .controller('DeliverablesCtrl', function ($scope, GridData, config, notifications) {
+    $scope.webServiceUrl = config.apiConfig.webServiceUrl;
+    $scope.pageChanged = function() {
+      $scope.loadPage($scope.currentPage);
+    };
+    $scope.loadPage = function(no) {
+      if(no === undefined) {
+        no = 1;
+      }
+      GridData('files').query({page: no}, function(resp) {
+        $scope.files = resp.items;
+        $scope.totalItems = resp.count;
+        $scope.currentPage = resp.page;
+      }, function(err) {
+        notifications.showError(err);
+      });
+    };
+    $scope.loadPage();
   });

b9d1bcc20c4eaf3c52372feb9eee5036ef2d6724
certeu <cert-eu@ec.europa.eu>
2019-11-27 15:32:25 +0100

    Add filtering to Deliverables Files page

--- app/scripts/controllers/deliverables.js
+++ app/scripts/controllers/deliverables.js
@@ -10,20 +10,40 @@
 angular.module('cpApp')
   .controller('DeliverablesCtrl', function ($scope, GridData, config, notifications) {
     $scope.webServiceUrl = config.apiConfig.webServiceUrl;
+    $scope.query = undefined;
+
     $scope.pageChanged = function() {
       $scope.loadPage($scope.currentPage);
     };
-    $scope.loadPage = function(no) {
-      if(no === undefined) {
+
+    $scope.filterPage = function() {
+      var query = $scope.search.trim();
+      if (query.length >= 3) {
+        $scope.query = query;
+      } else {
+        $scope.query = undefined;
+      }
+      $scope.loadPage();
+    };
+
+    $scope.loadPage = function(no){
+      if(no === undefined){
         no = 1;
       }
-      GridData('files').query({page: no}, function(resp) {
+      var params = {
+        page: no
+      };
+      if($scope.query !== undefined){
+        params.query = $scope.query;
+      }
+      GridData('files').query(params, function(resp){
         $scope.files = resp.items;
         $scope.totalItems = resp.count;
         $scope.currentPage = resp.page;
-      }, function(err) {
+      }, function(err){
         notifications.showError(err);
       });
     };
+
     $scope.loadPage();
   });
